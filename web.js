98a99f6f5f5afae77c1af766418712a1cfad47ce
sfabrizzio <sfabrizzio@hotmail.com>
2021-11-28 11:30:55 -0500

    Update web.js

--- web.js
+++ web.js
@@ -6,8 +6,8 @@ var app = express();
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-  res.send('Hello World!');
-});
+  res.send('Hola Mundo desde Herokud!');
+})
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
